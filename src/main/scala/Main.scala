fc69c6a1732842469b1990e1504faddd9993d861
mathhun <achemer@gmail.com>
2015-12-10 00:06:38 +0900

    snapshot

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,22 +1,55 @@
-class EmailCollection(val emails: Iterable[Email] = Nil) extends Iterable[Email] {
-  override def iterator = emails.iterator
+case class Summary(val emails: Seq[Email]) {
+  var tasks: Seq[Task] = Nil
 
-  override def filter(p: Email => Boolean): EmailCollection = {
-    new EmailCollection(emails.filter(p))
+  def summarize: Summary = {
+    this.tasks = this.emails.flatMap(e => Summary.extractTasks(e.body))
+    this
   }
 
-  def summarize(): Summary = {
-    new Summary
+  def report: Unit = {
+    //println(this.tasks)
   }
 }
 
-class Email(val headers: Map[String, String], val body: Seq[String]) {
+object Summary {
+  def extractTasks(body: Seq[String]): Seq[Task] = {
+    val chunk = body
+      .dropWhile(!_.matches(""".*\[チケット作業\].*"""))
+      .takeWhile(!_.matches("---+"))
+
+    val (tasks, rest) = chunk
+      .filter(ln => !ln.matches(""".*\[チケット外?作業\].*"""))
+      .partition(ln => ln.matches("""(#[0-9]+)? *(.*) *\([.0-9]+h\).*"""))
+
+    println("**********chunk***********")
+    println(chunk.mkString(","))
+
+    println("**********tasks***********")
+    println(tasks.mkString(","))
+
+    println("**********rest***********")
+    println(rest.mkString(","))
+
+    Seq(Task("", 0))
+  }
+}
+
+case class Task(name: String, hours: Double, ticketId: Int = 0) {
+}
+
+case class Email(headers: Map[String, String], body: Seq[String]) {
+  import java.util.{Date, Locale}
+
   val subject = headers("subject")
+  val date: Date = new Date(headers("date"))
   val ymd = Email.extractYearMonth(subject)
 
-  def year: Option[Int] = ymd.get('y)
-  def month: Option[Int] = ymd.get('m)
-  def day: Option[Int] = ymd.get('d)
+  def year: Int = ymd.getOrElse('y, -1)
+  def month: Int = ymd.getOrElse('m, -1)
+  def day: Int = ymd.getOrElse('d, -1)
+
+  override def toString: String =
+    "<Email subject='%s', date='%tF(%ta)'>".format(this.subject, this.date, this.date)
 }
 
 object Email {
@@ -26,12 +59,6 @@ object Email {
       case regex(y, m, d) => Map('y -> y.toInt, 'm -> m.toInt, 'd -> d.toInt)
       case _ => Map.empty
     }
-  }
-  def apply(headers: Map[String, String], body: Seq[String]) = new Email(headers, body)
-}
-
-class Summary {
-  def report: Unit = {
   }
 }
 
@@ -58,19 +85,18 @@ object Parser {
     m.toMap
   }
 
-  def parseEmail(s: String): Email = {
+  def parseEmail(s: String): Option[Email] = {
     val (headers, body) = """\r\n""".r.split(s).span(line => line != "")
-    Email(parseHeaders(headers.mkString("\n")), body.tail)
+    if (headers.length == 0) None
+    else Some(Email(parseHeaders(headers.mkString("\n")), body.tail))
   }
 
-  def parse(file: String): EmailCollection = {
+  def parse(file: String): Seq[Email] = {
     import java.io._
     import org.apache.commons.io.FileUtils
 
     val content = FileUtils.readFileToString(new File(file), "ISO-2022-JP")
-    val emails = split(content).init.map(parseEmail)
-    println(emails)
-    new EmailCollection
+    split(content).map(parseEmail).flatten
   }
 }
 
@@ -79,7 +105,7 @@ object Main {
     val file = args(0)
     val month = args(1).toInt
 
-    val emails = Parser.parse(file)
-    emails.filter(e => e.year == 2015 && e.month == month).summarize.report
+    val emails = Parser.parse(file).filter(e => e.year == 2015 && e.month == month)
+    Summary(emails).summarize.report
   }
 }
